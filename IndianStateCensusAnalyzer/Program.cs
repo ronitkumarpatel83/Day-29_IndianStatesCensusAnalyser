f69e7891439cfab92017873487a8dac4c2fb9796
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-01 16:31:51 +0530

    [Ronit] Added : Welcome Massage

---
+++ IndianStateCensusAnalyzer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser");
+            Console.ReadLine();
+        }
+    }
+}
